8d6846cb3153fcaf9a163e6a9232638811f90b93
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-01 13:42:50 -0400

    Update TuitionCondition.py
    Count how many times a word is used in a TextBlob

---
+++ TuitionCondition.py
@@ -0,0 +1,13 @@
+import re
+from textblob import TextBlob
+import random
+
+
+research = TextBlob("The the the cause you you you and and and we we we")
+
+seperate = research.words
+#turns the arguement into list by seperating each word
+
+research.word_counts
+#count how many times the word is repeated in the phrase
+print seperate and research.word_counts
