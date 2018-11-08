ab91c25c325d5f50556bcd92f04513ac764252a3
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-08 15:49:01 -0500

    Original Checkpoint for Python Project

--- TuitionCondition.py
+++ TuitionCondition.py
@@ -1,19 +1,13 @@
-import re
++import re
 from textblob import TextBlob
 import random
 
 
 research = TextBlob("The the the cause you you you and and and we we we")
 
-research.tags
-
 seperate = research.words
 #turns the arguement into list by seperating each word
 
-def popularNouns(sequence):
-    nouns = ()
-    for elements in sequence:
-        int(word.count([nouns for x in sequence if nouns > 3]))
-    return nouns
-
-popularNouns("The The The The The buy buy")
+research.word_counts
+#count how many times the word is repeated in the phrase
+print seperate and research.word_counts
