c41ecc30789da74d6d1dc34371b38a15ed3070f0
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-14 20:17:30 -0500

    Update TuitionCondition.py

--- TuitionCondition.py
+++ TuitionCondition.py
@@ -5,4 +5,4 @@ from textblob.taggers import NLTKTagger
 nltk_tagger = NLTKTagger()
 
 research = TextBlob("The the the cause you you you and and and we we we college run throw pay pay pay school money income aide", pos_tagger=nltk_tagger)
-research.pos_tags
+print research.pos_tags
