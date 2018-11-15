f5746a40b4eff34ffdd20e6a499f55d8e042c291
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-14 19:58:50 -0500

    Update TuitionCondition.py

--- TuitionCondition.py
+++ TuitionCondition.py
@@ -12,17 +12,8 @@ nouns = []
 verbs = []
 #create empty list for tagged words to be sent
 for tag in research:
-    if tag[1] == NN:
+    if tag[0] == NN:
         nouns.push
-    elif tag[1] == VB:
+    elif tag[0] == VB:
         verbs.push
-
-
-seperate = research.words
-#turns the arguement into list by seperating each word
-
-research.word_counts
-#count how many times the word is repeated in the phrase
-print research.word_counts
-print nouns
-print verbs
+    print (nouns, verbs)
