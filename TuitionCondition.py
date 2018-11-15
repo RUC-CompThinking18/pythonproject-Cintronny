2350bea3788ca2047286c0674a0d1e529f6c8a29
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-14 22:50:18 -0500

    Update TuitionCondition.py

--- TuitionCondition.py
+++ TuitionCondition.py
@@ -1,13 +1,17 @@
 import re
-from textblob import TextBlob
 import random
-from textblob.taggers import NLTKTagger
-nltk_tagger = NLTKTagger()
+#due to incredible lack of ability to solve my issues with understanding textblob I decided to remove that portion of my code all together.
+#As an adjustment I decided to take all of the tweets that I farmed on twitter and input them into a word cloud application to find repeated words.
+#I then scanned through the list of words that were repeated 3 or more times and piled them into a seperate list that I scanned to seperate the verbs, nouns, and adjectives.
 
-research = TextBlob("The the the cause you you you and and and we we we college run throw pay pay pay school money income aide", pos_tagger=nltk_tagger)
-research.pos_tags
-nouns = []
-verbs = []
-for tag in research.pos_tags:
-    if tag[1] == "NN":
-        nouns.apend(tag)
+nouns = ("tuition", "students", "college", "fees", "university", "semester", "school", "books", "class")
+verbs = ("pay", "will", "make", "fighting", "helps", "can", "access", "attending", "don't", "gonna", "learn")
+adj = ("free", "many", "new")
+
+num = random.choice(0,9)
+vernum = random.choice(0,11)
+adnum = random.choice(0,3)
+
+first_half = (adj[adnum] + ' ' + nouns[num] + ' ' + verbs[vernum])
+second_half = (adj[adnum] + ' ' + nouns[num])
+print first_half + ' ' + second_half
