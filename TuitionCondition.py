33c2f9fb0b2cc446b49d6d6e1b83b7c6ad00eba8
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-14 20:15:05 -0500

    nltk import

--- TuitionCondition.py
+++ TuitionCondition.py
@@ -1,19 +1,8 @@
 import re
 from textblob import TextBlob
 import random
+from textblob.taggers import NLTKTagger
 
 
-research = TextBlob("The the the cause you you you and and and we we we college run throw pay pay pay school money income aide")
-
-research.tags
-
-#tag each word with the type of word it is(noun, verb, etc.)
-nouns = []
-verbs = []
-#create empty list for tagged words to be sent
-for tag in research:
-    if tag[0] == NN:
-        nouns.push
-    elif tag[0] == VB:
-        verbs.push
-    print (nouns, verbs)
+research = TextBlob("The the the cause you you you and and and we we we college run throw pay pay pay school money income aide", pos_tagger=nltk_tagger)
+blob.pos_tags
