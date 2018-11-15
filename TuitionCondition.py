14d016c72123c4cc5fc365decfe668bbea86662b
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-14 22:52:47 -0500

    Update TuitionCondition.py

--- TuitionCondition.py
+++ TuitionCondition.py
@@ -8,9 +8,9 @@ nouns = ("tuition", "students", "college", "fees", "university", "semester", "sc
 verbs = ("pay", "will", "make", "fighting", "helps", "can", "access", "attending", "don't", "gonna", "learn")
 adj = ("free", "many", "new")
 
-num = random.choice(nouns)
-vernum = random.choice(verbs)
-adnum = random.choice(adj)
+num = random.randrange(0,9)
+vernum = random.randrange(0,11)
+adnum = random.randrange(0,3)
 
 first_half = (adj[adnum] + ' ' + nouns[num] + ' ' + verbs[vernum])
 second_half = (adj[adnum] + ' ' + nouns[num])
