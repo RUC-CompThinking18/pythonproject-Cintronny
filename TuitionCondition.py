c7e62d3974398145597d5e5176921d03b2e82cd6
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-11-26 14:02:31 -0500

    Finish Time Function and add stupid joke

--- TuitionCondition.py
+++ TuitionCondition.py
@@ -9,28 +9,27 @@ import time
 #As an adjustment I decided to take the top 30 tweets on that had the word "tuition" in it on twitter and input them into a word cloud application to find repeated words.
 #I then scanned through the list of words that were repeated 3 or more times and piled them into a seperate list that I scanned to seperate the verbs, nouns, and adjectives.
 
-nouns = ("tuition", "students", "college", "fees", "university", "semester", "school", "books", "class")
-verbs = ("pay", "will", "make", "fighting", "helps", "can", "access", "attending", "gonna", "learn")
-adj = ("free", "many", "new")
+nouns = ("tuition", "students", "college", "fees", "university", "semesters", "school", "books", "class", "loans", "dorm", "job", "")
+verbs = ("paying", "willing", "makes", "fighting", "helps", "can", "accesses", "attending", "gonna", "learning", "withdrawing", "failing", "get", "go",)
+adj = ("free", "many", "new", "expensive", "affordable", "rich", "poor", "stupid", "smarter", "tired")
 
 #The next few variables  are created to search through the lists above so that a random word will be chosen to develop the random generated sentance.
-num = random.randrange(0,8)
-num_two = random.randrange(0,8)
-vernum = random.randrange(0,9)
-adnum = random.randrange(0,2)
-adnum_two = random.randrange(0,2)
-#the randrange function allows the code to select a random word from each list.
-first_half = (adj[adnum] + ' ' + nouns[num] + ' ' + verbs[vernum])
-second_half = (adj[adnum_two] + ' ' + nouns[num_two])
 
-def create_tweet():
-    print(first_half + ' ' + second_half)
+def create_quote():
+    num = random.choice(nouns)
+    num_two = random.choice(nouns)
+    vernum = random.choice(verbs)
+    adnum = random.choice(adj)
+    adnum_two = random.choice(adj)
+    #the randrange function allows the code to select a random word from each list.
+    first_half = (adnum + ' ' + num + ' ' + vernum)
+    second_half = (adnum_two + ' ' + num_two)
+    print('"' + first_half + ' ' + second_half + '"')
+#I want this function to run every 15 seconds
 
-def countdown():
-    s=0
-    while s<=15:
-        time.sleep(1)
-        s+=1
-        if s==15:
-            create_tweet()
-            s=0
+quote = "Here is a quote from an incredibly stressed college student struggling with paying tuition:"
+
+while True:
+    print quote
+    create_quote()
+    time.sleep(15)
